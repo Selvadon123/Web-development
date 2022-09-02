0239497ac8963d49b76f3218795fbbb1c7079969
Selvadon123 <selvasuki1234@gmail.com>
2022-09-02 20:41:51 +0530

    First commit

---
+++ form.js
@@ -0,0 +1,38 @@
+function check_validity(){
+
+    var email = document.getElementById("email");
+    var u_name = document.getElementById("name");
+    var pass = document.getElementById("pass");
+
+
+    var email_val = false, name_val = false, pass_val = false;
+
+    var email_arr = ["selva@gmail.com",'sugan@gmail.com','kaus@gmail.com'];
+    var name_arr = ['selva','sugan','kaus'];
+
+    var pass_arr = [];
+
+    pass_arr["selva@gmail.com"] = "sk3";
+    pass_arr['sugan@gmail.com'] = 'sugan123';
+    pass_arr['kaus@gamil.com'] = 'kaus122';
+
+    if (email_arr.includes(email.value)){
+        email_val = true;
+        if (name_arr.includes(u_name.value)){
+            name_arr = true;
+            if (pass_arr[email.value] == pass.value){
+                pass_val = true;
+                alert("You have been logged in to the website");
+            }else{
+                alert("Invalid password");
+            }
+        }else{
+            alert('Invalid Name provided');
+        }
+    }else{
+        alert('Invalid Email address');
+        
+    }
+
+}
+
